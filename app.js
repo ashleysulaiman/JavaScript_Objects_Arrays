58f6ce9c71e29762c9247422df17994e19f3282c
Ashley Sulaiman <ashley.sulaiman83@gmail.com>
2021-07-08 11:35:45 -0400

    Completed Exercises

--- app.js
+++ app.js
@@ -3,7 +3,26 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
+const numbers = [2, 22, 12, 17, 18, 39, 129]
+function arraySum(numbers) {
+    let sum = 0;
+    numbers.forEach((num) => {sum += num;
+    });
+    return sum;
+};
+console.log(arraySum(numbers));
+
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+
+const book = {};
+book.title = "The Hobbit by J.R.R. Tolkien";
+book.pages = 295;
+book.readCount = 1;
+book.info = () => {
+    return `${book.title}, ${book.pages} pages, Read ${book.readCount} times.`;
+};
+console.log(book.info());
+
 
 
